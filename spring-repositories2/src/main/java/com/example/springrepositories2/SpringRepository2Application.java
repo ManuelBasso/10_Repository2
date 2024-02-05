f7cba02a2c5953ef5d7358b4cb8d5b260fd300f6
ManuelBasso <manuel.basso.89@gmail.com>
2024-02-05 16:11:49 +0100

    updated

--- spring-repositories2/src/main/java/com/example/springrepositories2/SpringRepository2Application.java
+++ spring-repositories2/src/main/java/com/example/springrepositories2/SpringRepository2Application.java
@@ -3,15 +3,11 @@ package com.example.springrepositories2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class SpringRepository2Application {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SpringRepository2Application.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "5050"));
-		app.run(args);
+		SpringApplication.run(SpringRepository2Application.class, args);
 	}
 
 }
